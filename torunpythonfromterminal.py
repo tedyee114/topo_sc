2def1c0a2fab3a2bbd5a4bbee8397b22132eb4b0
Ted Yee <tedyee114@gmail.com>
2023-11-07 12:08:28 -0500

    unable to make stylizing work on my computer, but some smoothing done

--- torunpythonfromterminal.py
+++ torunpythonfromterminal.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+now = datetime.now().strftime("%H:%M:%S")
+print('Step 00: Process Started at ', now)
 import sys                                                  #idk what this does but something to download ezdxf
 import subprocess                                           #gives access to the windows commandline
 import pkg_resources                                        #for downloading ezdxf
@@ -31,35 +34,38 @@ from ezdxf.addons import Importer                           #imports the importe
 # output_location = filedialog.askdirectory(initialdir = "\\", title = "Please Select the Output Folder for your contours")
 # print (output_location)
 # #endregion
-print('Step 0 Complete: Add-ons prepared,                      Now setting filepaths and reading files')
+print('Step 0 Complete: Add-ons prepared,                        **Now setting filepaths and reading files')
 
 #Input filepaths here
 global_mapper_exe=  "C:\\Program Files\\GlobalMapper23.1_64bit\\global_mapper.exe"
-script=             "C:\\Users\\AirWorksProcessing\\Documents\\Scripts\\topo_sc\\GMS\\Topo_Script1.gms"
-startfile=          "C:\\Users\\AirWorksProcessing\\Documents\\Scripts\\startfile.gmw"
-base_dxf=           ezdxf.readfile("C:\\Users\\AirWorksProcessing\\Documents\\Scripts\\v0_SCE_-_1597246_MERLIN_2023-07-27.dxf")
-contour_dxf=        ezdxf.readfile("C:\\Users\\AirWorksProcessing\\Documents\\Scripts\\output\\contour.dxf")
-final_location=     "C:\\Users\\AirWorksProcessing\\Documents\\Scripts\\merged.dxf"
+script=             "C:Documents\\Scripts\\topo_sc\\GMS\\Topo_Script1.gms"
+startfile=          "C:Documents\\Scripts\\startfile.gmw"
+base_dxf=           ezdxf.readfile("Documents\\Scripts\\v0_SCE_-_1597246_MERLIN_2023-07-27.dxf")
+contour_dxf=        ezdxf.readfile("Documents\\Scripts\\contour.dxf")
+final_location=     "Documents\\Scripts\\merged.dxf"
 
 # #Need to somehow pass the contour_output and other file locations to the script to it knows where to put the contours
 # #blah blah blah
 
-print('Step 1 Complete: Filepaths Set,                          Now opening startfile in GM and running toposcript...(this is the longest step and will take a few minutes)')
+print('Step 1 Complete: Filepaths Set,                           **Now opening startfile in GM and running toposcript...(this is the longest step and will take a few minutes)')
 
 #Uses Command Line to run Global Mapper, then the script you wrote
 # cmd = f'"{global_mapper_exe}" "{script}" '
 # subprocess.run(cmd, shell=True)
 
-print('Step 2 Complete: Toposcript complete, output in folder,  Now stylizing contour_dxf')
+print('Step 2 Complete: Toposcript complete, output in folder,   **Now stylizing contour_dxf')
 
 #changes layer names and colors
 doc = ezdxf.new(setup=True)  # setup required line types
 msp = doc.modelspace()
+for i in doc.layers:
+    print(doc.layers[i])
 if "Unknown_Line_Type" in doc.layers:
     obs_layer = contour_dxf.layers.get("Unknown_Line_Type")
     obs_layer.rename("A-OBSTRUCTION")
     obs_layer.dxf.linetype = "SOLID"
     obs_layer.color = 1                                                     #AutoCAD Color Index ACI Color System 1=red, 8=dark gray, 9=light gray
+    print('a')
 if "Unknown_Line_Type" in doc.layers:
     minr_layer = contour_dxf.layers.get("Contour_Line_Intermediate")
     minr_layer.rename("G-TOPO-MINR")
@@ -72,7 +78,7 @@ if "Unknown_Line_Type" in doc.layers:
     majr_layer.color = 9
 contour_dxf.save()
 
-print('Step 3 Complete: All processes complete,                  Now stylizing contour_DXF')
+print('Step 3 Complete: All processes complete,                 **Now merging back into base_dxf')
 
 #adding the generated contours to the base_file
 def merge(contour_dxf, base_dxf):
@@ -82,10 +88,10 @@ def merge(contour_dxf, base_dxf):
     # import all required resources and dependencies
     importer.finalize()
 
-merge(contour_dxf, base_dxf)                                           #runs the merge function defined above
-base_dxf.saveas(final_location)      #saves the new file   #base_dxf.save()   # to save as original filename
-
-print('Step 4 Complete: contour_dxf stylized, Now merging back into base_dxf')
+# merge(contour_dxf, base_dxf)                                           #runs the merge function defined above
+# base_dxf.saveas(final_location)      #saves the new file   #base_dxf.save()   # to save as original filename
+now = datetime.now().strftime("%H:%M:%S")
+print('All Processes Complete at '+now+'. base_dxf ready to open!')
 
 
 
